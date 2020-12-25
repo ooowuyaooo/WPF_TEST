73e027e89d6115a3bc9c51db4d58f9dac312972b
ooowuyaooo <netshare3322561@163.com>
2020-12-25 16:58:50 +0800

    Add project files.

---
+++ WpfApp1/Common/CommandBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.Common
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return DoCanExcute?.Invoke(parameter) == true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoExcute?.Invoke(parameter);
+        }
+
+        public Action<object> DoExcute { get; set; }
+
+        public Func<object,bool> DoCanExcute { get; set; }
+
+    }
+}
---
+++ WpfApp1/ViewModel/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp1.Common;
+
+namespace WpfApp1.ViewModel
+{
+    public class LoginViewModel
+    {
+        public CommandBase CloseWindowCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            this.CloseWindowCommand = new CommandBase();
+            this.CloseWindowCommand.DoExcute = new Action<object>((o) => {
+                (o as Window).Close();
+            });
+            this.CloseWindowCommand.DoCanExcute = new Func<object, bool>((o) => { return true; });
+        }
+    }
+
+
+}
+ 
